2540157c6c7aa8481154f9b475fb77a5bd1ae2d1
Liron Yahdav <lyahdav@users.noreply.github.com>
2017-01-08 20:59:21 -0800

    Refactor back and forward buttons and added refresh button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,35 +11,26 @@ import {
   WebView
 } from 'react-native';
 
-import { Entypo } from '@exponent/vector-icons';
+import { Entypo, Ionicons } from '@exponent/vector-icons';
 
 import { MonoText } from '../components/StyledText';
 
-class BackButton extends React.Component {
+class IconBarButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // TODO: probably cleaner way to do this
+    if (typeof(props.disabled) === "undefined") {
+      this.props.disabled = false;
+    }
+  }
+
   render() {
      return (
-       <TouchableOpacity style={styles.navBarButton} onPress={this._onPressButton}>
-         <Entypo name="chevron-left" size={32} />
+       <TouchableOpacity disabled={this.props.disabled} style={styles.navBarButton} onPress={this.props.onPress}>
+         <Entypo name={this.props.iconName} size={32} style={this.props.disabled ? styles.disabledBarButtonIcon : ''} />
        </TouchableOpacity>
      );
-  }
-
-  _onPressButton() {
-    homeScreenSingleton.webView.goBack();
-  }
-}
-
-class ForwardButton extends React.Component {
-  render() {
-     return (
-       <TouchableOpacity style={styles.navBarButton} onPress={this._onPressButton}>
-         <Entypo name="chevron-right" size={32} />
-       </TouchableOpacity>
-     );
-  }
-
-  _onPressButton() {
-    homeScreenSingleton.webView.goForward();
   }
 }
 
@@ -54,8 +45,54 @@ export default class HomeScreen extends React.Component {
   static route = {
     navigationBar: {
       title: 'Facebook',
-      renderLeft: (route, props) => <View style={styles.buttonContainer}><BackButton /><ForwardButton /></View>
+      renderLeft: (route, props) => homeScreenSingleton._renderNavBarLeft(),
+      renderRight: (route, props) => homeScreenSingleton._renderNavBarRight()
     },
+  }
+
+  // TODO: should initially be false, see onNavigationStateChange for more
+  state = {
+    backButtonEnabled: true,
+    forwardButtonEnabled: true,
+  };
+
+  _renderNavBarLeft() {
+    return (
+      <View style={styles.buttonContainer}>
+        <IconBarButton iconName="chevron-left" disabled={!this.state.backButtonEnabled} onPress={this._onPressBackButton}/>
+        <IconBarButton iconName="chevron-right" disabled={!this.state.forwardButtonEnabled} onPress={this._onPressForwardButton}/>
+      </View>
+    );
+  }
+
+  _renderNavBarRight() {
+    return (
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.navBarButton} onPress={this._onPressRefreshButton}>
+          <Ionicons name="ios-refresh" size={32} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  _onPressBackButton = () => {
+    this.webView.goBack();
+  }
+
+  _onPressForwardButton = () => {
+    this.webView.goForward();
+  }
+
+  _onPressRefreshButton = () => {
+    this.webView.reload();
+  }
+
+  _onNavigationStateChange = (navState) => {
+    // TODO: blocked by http://stackoverflow.com/questions/41512659/how-to-correctly-listen-to-navigation-changes-on-uiwebview
+    // this.setState({
+    //   backButtonEnabled: navState.canGoBack,
+    //   forwardButtonEnabled: navState.canGoForward,
+    // });
   }
 
   render() {
@@ -64,7 +101,8 @@ export default class HomeScreen extends React.Component {
         source={{uri: 'http://facebook.com'}}
         style={{marginTop: 20}}
         ref={(webView) => {this.webView = webView; }}
-      />
+        onNavigationStateChange={this._onNavigationStateChange}
+    />
     );
   }
 }
@@ -77,7 +115,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   navBarButton: {
-    height: 28,
-    width: 28,
+    height: 32,
+    width: 40,
   },
+  disabledBarButtonIcon: {
+    color: 'gray'
+  }
 });
