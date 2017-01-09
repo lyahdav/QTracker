3c32d74aec09cb89e03a7441ff06b2136884f181
Liron Yahdav <lyahdav@users.noreply.github.com>
2017-01-08 21:49:22 -0800

    Added doorbell.io integration for feedback

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -96,9 +96,22 @@ export default class HomeScreen extends React.Component {
   }
 
   render() {
+    const doorbellJS = `
+        window.doorbellOptions = {
+            appKey: 'q87dPNLHiGVntOR7D1WM51Mwf35XWysjXEeSTE2sVy7K5BzYgMCCJQkwuoYDBEaR'
+        };
+        (function(w, d, t) {
+            var hasLoaded = false;
+            function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/5139?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
+            if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
+            if (d.readyState == 'complete') { l(); }
+        }(window, document, 'script'));
+    `.trim();
+    
     return (
       <WebView
         source={{uri: 'http://facebook.com'}}
+        injectedJavaScript={doorbellJS}
         style={{marginTop: 20}}
         ref={(webView) => {this.webView = webView; }}
         onNavigationStateChange={this._onNavigationStateChange}
